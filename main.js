41846c3c71ab6daa1125850291b8e43ff64265c2
JERSEY JACKPOT <JERSEYJACKPOT@GMAIL.COM>
2020-07-06 18:46:20 -0400

    added comments to code

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
 $(window).on("load", function () {
   var currentDate;
   var currentTime;
@@ -5,7 +7,8 @@ $(window).on("load", function () {
   currentDate = moment().format("dddd MMM Do YYYY, h:mm a");
   $("#currentDay").append(currentDate);
   currentTime = parseInt(moment().format("H"));
-
+  // WHEN I view the timeblocks for that day
+  // THEN each timeblock is color coded to indicate whether it is in the past, present, or future
   var displaySavedAppt = function () {
     for (i = 9; i <= 17; i++) {
       $("#" + i).removeClass();
@@ -17,7 +20,9 @@ $(window).on("load", function () {
         $("#" + i).addClass("row future");
       }
     }
-
+    // THEN the text for that event is saved in local storage
+    // WHEN I refresh the page
+    // THEN the saved events persist
     for (var hour = 9; hour <= 17; hour++) {
       $("#" + hour)
         .children(".textarea")
@@ -26,7 +31,9 @@ $(window).on("load", function () {
     }
   };
   displaySavedAppt();
-
+  // WHEN I click into a timeblock
+  // THEN I can enter an event
+  // WHEN I click the save button for that timeblock
   $(".saveIcon").click(function () {
     var text = $(this).siblings(".textarea").val();
     console.log(text);
